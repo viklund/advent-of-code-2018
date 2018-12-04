d4095d1cf4d7f6bfd4908692409ab663e2ee3861
Johan Viklund <johan.viklund@nbis.se>
2018-12-04 18:04:18 +0100

    Solution and input data for problem 1-1

---
+++ 01-1.rs
@@ -0,0 +1,20 @@
+//use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+use std::io;
+
+fn main() -> io::Result<()> {
+    let f = File::open("01-1.input")?;
+    let reader = BufReader::new(f);    
+
+    let mut n = 0;
+
+    for line in reader.lines() {
+        let num = i32::from_str_radix(&line?, 10).unwrap();
+        n += num;
+    }
+
+    println!("Result: {}", n);
+    Ok(())
+}
