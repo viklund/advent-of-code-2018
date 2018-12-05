726298d2e71b71439495b4aa921af75782cde969
Johan Viklund <johan.viklund@nbis.se>
2018-12-05 12:58:19 +0100

    Day 2 part 2

---
+++ src/bin/02-2.rs
@@ -0,0 +1,49 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+use std::io;
+use std::vec::Vec;
+
+fn main() -> io::Result<()> {
+    let f = File::open("data/02-1.input")?;
+    let reader = BufReader::new(f);    
+
+    let mut ids : Vec<Vec<char>> = Vec::new();
+    let mut lnum = 0;
+
+    'line: for line in reader.lines() {
+        lnum += 1;
+        let mut chars = Vec::new();
+        for c in line?.chars() {
+            chars.push(c);
+        }
+
+        'id: for id in ids.clone() {
+            let mut ndiff = 0;
+            for ii in 0..chars.len() {
+                if chars[ii] != id[ii] {
+                    ndiff += 1;
+
+                    if ndiff > 2 {
+                        continue 'id;
+                    }
+                }
+            }
+
+            if ndiff == 1 {
+                println!("Found it, at: {}", lnum);
+                for ii in 0..chars.len() {
+                    if chars[ii] == id[ii] {
+                        print!("{}", chars[ii]);
+                    }
+                }
+                println!();
+                break 'line;
+            }
+        }
+
+        ids.push( chars );
+    }
+
+    Ok(())
+}
